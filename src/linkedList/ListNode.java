0f877755e43634effdef7f613a8eb26d8278207e
Sandesh Dahake <sandeshdahake@gmail.com>
2021-12-02 23:37:58 +0100

    Create ListNode.java

---
+++ src/linkedList/ListNode.java
@@ -0,0 +1,14 @@
+package linkedList;
+
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+
+    public ListNode() {
+
+    }
+}
