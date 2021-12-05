b9c67818f202fb793f3f2a6f2b355d855ae38b02
Sandesh Dahake <sandeshdahake@gmail.com>
2021-12-05 23:52:43 +0100

    Create Merge_Two_Sorted_Lists.java

---
+++ src/linkedList/Merge_Two_Sorted_Lists.java
@@ -0,0 +1,56 @@
+package linkedList;
+
+/**
+ * https://leetcode.com/explore/learn/card/linked-list/213/conclusion/1227/
+ * ou are given the heads of two sorted linked lists list1 and list2.
+ *
+ * Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.
+ *
+ * Return the head of the merged linked list.
+ *
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: list1 = [1,2,4], list2 = [1,3,4]
+ * Output: [1,1,2,3,4,4]
+ * Example 2:
+ *
+ * Input: list1 = [], list2 = []
+ * Output: []
+ * Example 3:
+ *
+ * Input: list1 = [], list2 = [0]
+ * Output: [0]
+ *
+ *
+ * Constraints:
+ *
+ * The number of nodes in both lists is in the range [0, 50].
+ * -100 <= Node.val <= 100
+ * Both list1 and list2 are sorted in non-decreasing order.
+ */
+public class Merge_Two_Sorted_Lists {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode head = new ListNode();
+        ListNode node = head;
+
+        while(list1 != null && list2 != null) {
+            if(list1.val <= list2.val) {
+                node.next = list1;
+                node = list1;
+                list1 = list1.next;
+            } else {
+                node.next = list2;
+                node = list2;
+                list2 = list2.next;
+            }
+        }
+        if(list1 != null || list2 != null) {
+            node.next = list1 != null ? list1 : list2;
+        }
+
+        return head.next;
+    }
+}
