083b43b978827d1f91bf347d8d96eea1a1599155
Sandesh Dahake <sandeshdahake@gmail.com>
2021-12-28 15:32:57 +0100

    Update House_Robber2.java

--- src/algo/dp/House_Robber2.java
+++ src/algo/dp/House_Robber2.java
@@ -7,18 +7,23 @@ class House_Robber2 {
     public int rob(int[] nums) {
         if(nums == null || nums.length == 0) return 0; 
         
-        if(nums.length == 1) return nums[0];
+        if(nums.length == 1) return nums[0]; 
+        if(nums.length == 2) return Math.max(nums[0], nums[1]);
         
-        if(nums.length == 2) return Math.max(nums[0],nums[1]);
+        int rob1 = letsRob(nums, 0, nums.length -2); 
+        int rob2 = letsRob(nums, 1, nums.length -1); 
         
-        int[] dp = new int[nums.length];
-        dp[0] = nums[0];
-        dp[1] = Math.max(nums[0], nums[1]);
+        return Math.max(rob1, rob2); 
+    }
+    
+    private int letsRob(int[] nums, int startIndex, int lastIndex){
         
-        for(int i=2; i< nums.length; i++){
-            dp[i] = Math.max(nums[i]+dp[i-2], dp[i-1]);
+        int[] table = new int[nums.length];
+        table[startIndex] = nums[startIndex]; 
+        table[startIndex+1] = Math.max(nums[startIndex], nums[startIndex+1]);
+        for(int i= startIndex+2; i<= lastIndex; i++){
+          table[i] = Math.max(table[i-1], table[i-2] + nums[i]);    
         }
-        
-        return dp[nums.length-1];
+        return table[lastIndex]; 
     }
 }
