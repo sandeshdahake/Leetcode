4f7f35e33cbd7cbe60363dfbb8102b9916a97bf8
Sandesh Dahake <sandeshdahake@gmail.com>
2022-01-05 19:59:56 +0100

    Create Palindrome_Partitioning.java

---
+++ src/algo/dp/Palindrome_Partitioning.java
@@ -0,0 +1,48 @@
+package algo.dp;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Given a string s, partition s such that every substring of the partition is a palindrome. Return all possible palindrome partitioning of s.
+ *
+ * A palindrome string is a string that reads the same backward as forward.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: s = "aab"
+ * Output: [["a","a","b"],["aa","b"]]
+ * Example 2:
+ *
+ * Input: s = "a"
+ * Output: [["a"]]
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= s.length <= 16
+ * s contains only lowercase English letters.
+ */
+public class Palindrome_Partitioning {
+    public List<List<String>> partition(String s) {
+        int len = s.length();
+        boolean[][] dp = new boolean[len][len];
+        List<List<String>> result = new ArrayList<>();
+        dfs(result, s, 0, new ArrayList<>(), dp);
+        return result;
+    }
+
+    void dfs(List<List<String>> result, String s, int start, List<String> currentList, boolean[][] dp) {
+        if (start >= s.length()) result.add(new ArrayList<>(currentList));
+        for (int end = start; end < s.length(); end++) {
+            if (s.charAt(start) == s.charAt(end) && (end - start <= 2 || dp[start + 1][end - 1])) {
+                dp[start][end] = true;
+                currentList.add(s.substring(start, end + 1));
+                dfs(result, s, end + 1, currentList, dp);
+                currentList.remove(currentList.size() - 1);
+            }
+        }
+    }
+}
