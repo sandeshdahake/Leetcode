83764f856be9d2c35d6048fd82adde2e1c48b5df
Sandesh Dahake <sandeshdahake@gmail.com>
2022-01-08 23:35:12 +0100

    Create Cherry_Pickup_II.java

---
+++ src/algo/dp/Cherry_Pickup_II.java
@@ -0,0 +1,79 @@
+package algo.dp;
+
+/**
+ * You are given a rows x cols matrix grid representing a field of cherries where grid[i][j] represents the number of cherries that you can collect from the (i, j) cell.
+ *
+ * You have two robots that can collect cherries for you:
+ *
+ * Robot #1 is located at the top-left corner (0, 0), and
+ * Robot #2 is located at the top-right corner (0, cols - 1).
+ * Return the maximum number of cherries collection using both robots by following the rules below:
+ *
+ * From a cell (i, j), robots can move to cell (i + 1, j - 1), (i + 1, j), or (i + 1, j + 1).
+ * When any robot passes through a cell, It picks up all cherries, and the cell becomes an empty cell.
+ * When both robots stay in the same cell, only one takes the cherries.
+ * Both robots cannot move outside of the grid at any moment.
+ * Both robots should reach the bottom row in grid.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: grid = [[3,1,1],[2,5,1],[1,5,5],[2,1,1]]
+ * Output: 24
+ * Explanation: Path of robot #1 and #2 are described in color green and blue respectively.
+ * Cherries taken by Robot #1, (3 + 2 + 5 + 2) = 12.
+ * Cherries taken by Robot #2, (1 + 5 + 5 + 1) = 12.
+ * Total of cherries: 12 + 12 = 24.
+ * Example 2:
+ *
+ *
+ * Input: grid = [[1,0,0,0,0,0,1],[2,0,0,0,0,3,0],[2,0,9,0,0,0,0],[0,3,0,5,4,0,0],[1,0,2,3,0,0,6]]
+ * Output: 28
+ * Explanation: Path of robot #1 and #2 are described in color green and blue respectively.
+ * Cherries taken by Robot #1, (1 + 9 + 5 + 2) = 17.
+ * Cherries taken by Robot #2, (1 + 3 + 4 + 3) = 11.
+ * Total of cherries: 17 + 11 = 28.
+ *
+ *
+ * Constraints:
+ *
+ * rows == grid.length
+ * cols == grid[i].length
+ * 2 <= rows, cols <= 70
+ * 0 <= grid[i][j] <= 100
+ */
+public class Cherry_Pickup_II {
+    public int cherryPickup(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int dp[][][] = new int[m][n][n];
+
+        for (int row = m - 1; row >= 0; row--) {
+            for (int col1 = 0; col1 < n; col1++) {
+                for (int col2 = 0; col2 < n; col2++) {
+                    int result = 0;
+                    // current cell
+                    result += grid[row][col1];
+                    if (col1 != col2) {
+                        result += grid[row][col2];
+                    }
+                    // transition
+                    if (row != m - 1) {
+                        int max = 0;
+                        for (int newCol1 = col1 - 1; newCol1 <= col1 + 1; newCol1++) {
+                            for (int newCol2 = col2 - 1; newCol2 <= col2 + 1; newCol2++) {
+                                if (newCol1 >= 0 && newCol1 < n && newCol2 >= 0 && newCol2 < n) {
+                                    max = Math.max(max, dp[row + 1][newCol1][newCol2]);
+                                }
+                            }
+                        }
+                        result += max;
+                    }
+                    dp[row][col1][col2] = result;
+                }
+            }
+        }
+        return dp[0][0][n - 1];
+    }
+}
